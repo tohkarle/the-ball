826d3b32438cf13f712a52c0254c8f78af75470d
tohkarle <tohkarle@hotmail.com>
2024-02-24 23:58:37 +0800

    Initial Commit

---
+++ TheBall/TheBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBallApp.swift
+//  TheBall
+//
+//  Created by Toh Kar Le on 24/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TheBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
